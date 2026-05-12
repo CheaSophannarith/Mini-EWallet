551aa073949a5d3805d952c76557b1326e869b9a
LaFii <sophannarith38@gmail.com>
2026-05-12 15:19:30 +0700

    Add WalletNotFoundException class for handling wallet-related errors

---
+++ src/main/java/com/fii/ewallet/exception/WalletNotFoundException.java
@@ -0,0 +1,7 @@
+package com.fii.ewallet.exception;
+
+public class WalletNotFoundException extends RuntimeException {
+  public WalletNotFoundException(String message) {
+    super(message);
+  }
+}
